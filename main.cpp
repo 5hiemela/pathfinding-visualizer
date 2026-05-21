788d051c0539260ce7927e9c684dfda1e063b9ff
Chiemela <onyensochiemela24@gmail.com>
2026-05-21 02:18:48 -0500

    Setup GLFW + ImGui + OpenGL base

--- main.cpp
+++ main.cpp
@@ -1,19 +1,51 @@
-#include <SFML/Graphics.hpp>
+#include <imgui.h>
+#include "backends/imgui_impl_glfw.h"
+#include "backends/imgui_impl_opengl3.h"
+
+#include <GLFW/glfw3.h>
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode({800, 600}), "Pathfinding Visualizer");
+    glfwInit();
 
-    while (window.isOpen()) {
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Pathfinding Visualizer", nullptr, nullptr);
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
 
-        while (const std::optional event = window.pollEvent()) {
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO();
 
-            if (event->is<sf::Event::Closed>())
-                window.close();
-        }
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 130");
 
-        window.clear();
-        window.display();
+    while (!glfwWindowShouldClose(window)) {
+
+        glfwPollEvents();
+
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        ImGui::Begin("Control Panel");
+        ImGui::Text("GLFW + ImGui is working");
+        ImGui::Button("Test");
+        ImGui::End();
+
+        ImGui::Render();
+
+        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        glfwSwapBuffers(window);
     }
+
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
 
     return 0;
 }
