4134758dac7cdde2cd59254f3ae5950566027098
Chiemela <onyensochiemela24@gmail.com>
2026-05-26 00:22:11 -0500

    Render initial grid in OpenGL

--- main.cpp
+++ main.cpp
@@ -3,6 +3,31 @@
 #include "backends/imgui_impl_opengl3.h"
 
 #include <GLFW/glfw3.h>
+#include <vector>
+
+struct Cell {
+    int x, y;
+    bool isWall = false;
+};
+
+const int GRID_WIDTH = 50;
+const int GRID_HEIGHT = 30;
+
+std::vector<std::vector<Cell>> grid;
+
+const float CELL_SIZE = 0.04f;
+
+void drawCell(float x, float y, float size) {
+
+    glBegin(GL_LINE_LOOP);
+
+    glVertex2f(x, y);
+    glVertex2f(x + size, y);
+    glVertex2f(x + size, y - size);
+    glVertex2f(x, y - size);
+
+    glEnd();
+}
 
 int main() {
     glfwInit();
@@ -16,6 +41,14 @@ int main() {
 
     ImGui_ImplGlfw_InitForOpenGL(window, true);
     ImGui_ImplOpenGL3_Init("#version 130");
+
+    grid.resize(GRID_HEIGHT);
+
+    for (int y = 0; y < GRID_HEIGHT; y++) {
+        for (int x = 0; x < GRID_WIDTH; x++) {
+            grid[y].push_back(Cell{x, y, false});
+        }
+    }
 
     while (!glfwWindowShouldClose(window)) {
 
@@ -34,6 +67,17 @@ int main() {
 
         glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        for (int y = 0; y < GRID_HEIGHT; y++) {
+
+            for (int x = 0; x < GRID_WIDTH; x++) {
+
+                float screenX = -1.0f + (x * CELL_SIZE);
+                float screenY =  1.0f - (y * CELL_SIZE);
+
+                drawCell(screenX, screenY, CELL_SIZE);
+            }
+        }
 
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
